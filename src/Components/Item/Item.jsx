a04c8acb66f4f79fc0c797d611b8106e7a32f61b
LucasPulgaci032 <163359576+LucasPulgaci032@users.noreply.github.com>
2024-12-06 15:51:01 -0300

    Add files via upload

---
+++ src/Components/Item/Item.jsx
@@ -0,0 +1,19 @@
+
+import './item.style.css';
+function Item({ item }) {
+  const { name, sprites } = item;
+    const sprite = sprites.other['official-artwork'].front_default;
+
+  
+    return (
+      <li className="list-item">
+            <div className="list-item-image-wrapper">
+                <img alt={name} className="list-item-image" src={sprite} />
+            </div>
+            <p className='item-name'>{name}</p>
+        </li>
+    );
+  }
+  
+  export default Item;
+  
